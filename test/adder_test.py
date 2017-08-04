1477495c05e814331da3a1678e15b0463a02b12d
webf0x@hotmail.com <webf0x@hotmail.com>
2017-08-04 15:04:56 -0400

    Create adder in method shared by tests

--- test/adder_test.py
+++ test/adder_test.py
@@ -3,17 +3,18 @@ import unittest
 
 
 class AdderTest(unittest.TestCase):
-    def test_adder_learned_to_add(self):
-        adder = adder_ai.AdderAI()
-        adder.train()
+    def setUp(self):
+        self.adder = adder_ai.AdderAI()
 
-        self.assertAlmostEquals(1, adder.add(1, 0), delta=0.1)
-        self.assertAlmostEquals(1, adder.add(0, 1), delta=0.1)
-        self.assertAlmostEquals(0, adder.add(0, 0), delta=0.1)
-        self.assertAlmostEquals(5, adder.add(2, 3), delta=0.1)
-        self.assertAlmostEquals(-5, adder.add(-2, -3), delta=0.1)
+    def test_adder_learns_to_add(self):
+        self.adder.train()
 
-    def test_adder_can_train_twice_without_breaking(self):
-        adder = adder_ai.AdderAI()
-        adder.train()
-        adder.train()
+        self.assertAlmostEquals(1, self.adder.add(1, 0), delta=0.1)
+        self.assertAlmostEquals(1, self.adder.add(0, 1), delta=0.1)
+        self.assertAlmostEquals(0, self.adder.add(0, 0), delta=0.1)
+        self.assertAlmostEquals(5, self.adder.add(2, 3), delta=0.1)
+        self.assertAlmostEquals(-5, self.adder.add(-2, -3), delta=0.1)
+
+    def test_adder_can_train_twice(self):
+        self.adder.train()
+        self.adder.train()
